bac06e7c68da1f195cbea5b69290190784a6247f
Adriana_fajardo <afajardo@est.ups.edu.ec>
2024-11-27 09:07:36 -0500

    Add(I): Square  Implementation

--- src/figures/Main.java
+++ src/figures/Main.java
@@ -1,20 +1,23 @@
 import figures.Line;
+import figures.Square;
 
 import java.util.Scanner;
 
-public class Main {
+public class Main{
     public static void main(String[] args) {
         int option = 0;
         Scanner scanner = new Scanner(System.in);
         Line line = new Line();
         do{
             System.out.println("1. Create Line");
-            System.out.println("2. Print Line");
-            System.out.println("3. Exit");
+            System.out.println("2. Create Square");
+            System.out.println("3. Print Line");
+            System.out.println("4. Print Square");
+            System.out.println("5. Exit");
             option=scanner.nextInt();
             switch (option){
                 case 1:
-                    System.out.println("Please enter length: ");
+                    System.out.println("Please enter : ");
                     int lineLength = scanner.nextInt();
                     line.setLength(lineLength);
                     break;
